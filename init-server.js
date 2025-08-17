cd08f83bd8d88d45d0d0a38e3ba8f0e33e2c6568
Amaterasu <caletina.marklester@gmail.com>
2025-08-17 14:32:08 +0800

    adding server for vercel

--- init-server.js
+++ init-server.js
@@ -4,7 +4,7 @@ const rateLimit = require("@fastify/rate-limit");
 
 const fastify = Fastify({ logger: true });
 
-async function start() {
+async function buildServer() {
   await fastify.register(corsPlugin);
 
   fastify.register(rateLimit, {
@@ -14,12 +14,27 @@ async function start() {
 
   fastify.register(require("./routes/"), { prefix: "/v1" });
 
+  return fastify;
+}
+
+async function startLocal() {
+  await buildServer();
   try {
     await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    console.log("Server running locally at http://localhost:3000");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 }
 
-start();
+if (process.env.VERCEL) {
+  module.exports = async (req, res) => {
+    await buildServer();
+    await fastify.ready();
+    fastify.server.emit("request", req, res);
+  };
+} else {
+  // Local server
+  startLocal();
+}
