d11594af1417362da6a7949a797df6012c512725
Amaterasu <caletina.marklester@gmail.com>
2025-08-17 14:38:52 +0800

    modifying init server

--- init-server.js
+++ init-server.js
@@ -3,38 +3,61 @@ const corsPlugin = require("./configs/cors.config");
 const rateLimit = require("@fastify/rate-limit");
 
 const fastify = Fastify({ logger: true });
-
-// Flag to make sure we only register plugins once
 let isServerBuilt = false;
 
 async function buildServer() {
-  if (!isServerBuilt) {
-    await fastify.register(corsPlugin);
+  if (isServerBuilt) {
+    await fastify.ready();
+    return fastify;
+  }
 
+  try {
+    await fastify.register(corsPlugin);
     fastify.register(rateLimit, {
       max: 100,
       timeWindow: "1 minute",
     });
-
     fastify.register(require("./routes/"), { prefix: "/v1" });
 
     isServerBuilt = true;
+    await fastify.ready();
+    fastify.log.info("Fastify server built successfully");
+    return fastify;
+  } catch (err) {
+    fastify.log.error("Error building Fastify server:", err);
+    throw err;
   }
-
-  await fastify.ready();
-  return fastify;
 }
 
 if (!process.env.VERCEL) {
-  buildServer().then(() => {
-    fastify.listen({ port: 3000, host: "0.0.0.0" }).catch((err) => {
-      fastify.log.error(err);
+  buildServer()
+    .then(() => {
+      fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+        if (err) {
+          fastify.log.error("Error starting server:", err);
+          process.exit(1);
+        }
+        fastify.log.info(`Server running locally at ${address}`);
+      });
+    })
+    .catch((err) => {
+      fastify.log.error("Failed to build server locally:", err);
       process.exit(1);
     });
-  });
 }
 
 module.exports = async (req, res) => {
-  const server = await buildServer();
-  server.server.emit("request", req, res);
+  try {
+    const server = await buildServer();
+    await new Promise((resolve, reject) => {
+      server.server.emit("request", req, res);
+      res.on("finish", resolve);
+      res.on("error", reject);
+    });
+  } catch (err) {
+    console.error("Serverless handler error:", err);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "Internal Server Error" }));
+  }
 };
