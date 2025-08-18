5f3bbc40ea73f56f9b89bb216f62fda33ca9bf3d
Jonas <j.tulang@satelliteteams.com>
2025-08-18 21:35:51 +0800

    added hook for token verification

--- init-server.js
+++ init-server.js
@@ -1,11 +1,15 @@
 const Fastify = require("fastify");
 const cors = require("@fastify/cors");
 const rateLimit = require("@fastify/rate-limit");
-
+const verify = require("./middleware/middleware");
 const fastify = Fastify();
 
+fastify.register(jwt, {
+  secret: config.auth_secret
+});
+
 // Register CORS
-await fastify.register(corsPlugin);
+fastify.register(corsPlugin);
 
 // Register rate limit
 fastify.register(rateLimit, {
@@ -15,6 +19,12 @@ fastify.register(rateLimit, {
 
 fastify.register(require("./routes/"));
 
+fastify.addHook('preHandler', async (request, reply) => {
+  const url = request.url;
+  const excludedPaths = []; // excluded route
+  if (excludedPaths.includes(`${url}`)) return;
+  await verify(fastify, request, reply);
+});
 
 fastify.listen({ port: 3000 });
 console.log('Server listening on http://localhost:3000')
