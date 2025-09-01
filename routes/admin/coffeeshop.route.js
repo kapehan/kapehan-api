a64dc7b3d3653079776392fcc7abfbbc405ffa6d
Jonas <j.tulang@satelliteteams.com>
2025-09-01 23:36:35 +0800

    api for select of coffeeshop modified end point of create coffeeshop

--- routes/admin/coffeeshop.route.js
+++ routes/admin/coffeeshop.route.js
@@ -1,4 +1,5 @@
 const coffee_shop = require("../../controllers/coffeeshop/coffeeshop.controller")
 module.exports.coffeeshop = (app) => {
-    app.post('/coffee-shop', coffee_shop.create)
+    app.post('/coffee-shop/create', coffee_shop.create),
+    app.post('/coffee-shop', coffee_shop.find)
 }
