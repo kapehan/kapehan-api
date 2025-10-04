cdef3d4367c19dbddf235c20757513766e7e4216
Jonas <j.tulang@satelliteteams.com>
2025-10-04 18:19:10 +0800

    added useraccess and middleware

--- routes/admin/coffeeshop.route.js
+++ routes/admin/coffeeshop.route.js
@@ -1,7 +1,26 @@
 const coffee_shop = require("../../controllers/coffeeshop/coffeeshop.controller")
+const { authMiddleware } = require("../../middleware/middleware.js");
+const { AccessLevels } = require("../../utils/accessLevels.js");
+
 module.exports.coffeeshop = (app) => {
-    app.post('/coffee-shop/create', coffee_shop.create);
-    app.post('/coffee-shop', coffee_shop.find);
-    app.post('/coffee-shop/:id/update', coffee_shop.update);
-    app.post('/coffee-shop/:id/delete', coffee_shop._delete);
+    app.post('/coffee-shop/create',
+    {
+      preHandler: authMiddleware,
+      config: { access: AccessLevels.ADMIN }, // Only admin can access
+    }, coffee_shop.create);
+    app.post('/coffee-shop',
+    {
+      preHandler: authMiddleware,
+      config: { access: AccessLevels.ADMIN }, // Only admin can access
+    }, coffee_shop.find);
+    app.post('/coffee-shop/:id/update',
+    {
+      preHandler: authMiddleware,
+      config: { access: AccessLevels.ADMIN }, // Only admin can access
+    }, coffee_shop.update);
+    app.post('/coffee-shop/:id/delete',
+    {
+      preHandler: authMiddleware,
+      config: { access: AccessLevels.ADMIN }, // Only admin can access
+    }, coffee_shop._delete);
 }
